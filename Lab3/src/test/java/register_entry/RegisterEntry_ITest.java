514958fc6fdce1aab511fddd32a14bb07fdf8a9d
asifwasefi <asifwasefi@yahoo.com>
2020-12-08 22:13:49 +0100

    Exercise 3 complete
    Exercise 3: We have to make at least one integration test.
    
    I made one which tests the hours, minutes and seconds at which certaint employee is checked in or checked out.

---
+++ Lab3/src/test/java/register_entry/RegisterEntry_ITest.java
@@ -0,0 +1,56 @@
+package register_entry;
+
+
+import database.RegistrationDB;
+import employee.Employee;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+public class RegisterEntry_ITest {
+
+    int hours;int minutes;int seconds;
+
+    public RegisterEntry_ITest() {
+    }
+
+    @Before
+    public void initialize()
+    {
+        System.out.println("Initializing integration test");
+        RegistrationDB db = RegistrationDB.getInstance();
+        Employee e = new Employee("Adam","designer");
+        db.addEntry(e,new RegisterEntry(true));
+        RegisterEntry databaseEntry = db.getEntry(e);
+        this.hours = databaseEntry.getHours();
+        this.minutes = databaseEntry.getMinutes();
+        this.seconds = databaseEntry.getSeconds();
+
+    }
+
+    @Test
+    public void t_getEntryHours()
+    {
+        RegisterEntry entry = new RegisterEntry(true);
+        int hours2 = entry.getHours();
+        assertEquals("Testing hours at which the entry for a certain employee is added to database",hours2,hours);
+    }
+
+    @Test
+    public void t_getEntryMinutes()
+    {
+        RegisterEntry entry = new RegisterEntry(true);
+        int minutes2 = entry.getMinutes();
+        assertEquals("Testing minutes at which the entry for a certain employee is added to database",minutes2,minutes);
+    }
+
+    @Test
+    public void t_getEntrySeconds()
+    {
+        RegisterEntry entry = new RegisterEntry(true);
+        int seconds2 = entry.getSeconds();
+        assertEquals("Testing hours at which the entry for a certain employee is added to database",seconds2,seconds);
+    }
+
+}
